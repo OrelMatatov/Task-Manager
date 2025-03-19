124e4ed61ad004afdc698ba8f024d1d57b2afbd1
Orel Matatov <orel.matatov@gmail.com>
2025-03-19 16:52:21 +0200

    add server side dependencies

--- server/server.js
+++ server/server.js
@@ -7,7 +7,16 @@ const users = require('./routes/users');
 
 app.use(express.json());
 
+/* const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+
+app.use(logger); */
+
 app.use('/tasks', tasks);
 app.use('/users', users);
+
+
 
 app.listen(8000, () => console.log("Server is running on port 8000"));
